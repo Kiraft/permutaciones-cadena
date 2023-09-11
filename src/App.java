94ed25a78236fe5b44e055ff63751b52af25dec3
Jonathan <joniosva@gmail.com>
2023-09-10 21:57:16 -0600

    Agrege w2

--- src/App.java
+++ src/App.java
@@ -1,14 +1,36 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
-        String input = "xyzf";
+        Scanner sc = new Scanner(System.in);
+        
+        // System.out.println("Ingresa la cadena: ");
+        // String input = sc.nextLine();
+        String input = "xyz";
+        
+        ArrayList<String> w1 = generarPermutaciones(input);
+        ArrayList<String> w2 = new ArrayList<>();
 
         // Imprimimos los valores
-        generarPermutaciones(input).forEach((permutacion) -> {
-            System.out.print(permutacion + ", ");
+        System.out.println("--- VALORES DE W1 --- ");
+        w1.forEach((permutacion) -> {
+            System.out.print(permutacion + " ");
+        });
+        System.out.println("\n");
+        
+        // Llena w2 con valores de w1
+        for (String permutacion : w1) {
+            for (String permutacionAux : w1) {
+                w2.add(permutacion.concat(permutacionAux));
+            }
+        }
+
+        // Imprime valores de w2
+        System.out.println("--- VALORES DE W2 ---");
+        w2.forEach((permutacion) -> {
+            System.out.print(permutacion + " ");
         });
 
     }
@@ -16,39 +38,46 @@ public class App {
     public static ArrayList<String> generarPermutaciones(String input) {
 
         // Esta estructura de datos no acepta repeticiones de nuestro input
-        HashSet<Character> set = new HashSet<>();
+        HashSet<Character> setCaracters = new HashSet<>();
 
         //LLena el set con cada caracter de input
         for (char c : input.toCharArray()) {
-            set.add(c);
+            setCaracters.add(c);
         }
 
-        ArrayList<String> result = new ArrayList<>();
+        // Guarda los resultados
+        ArrayList<String> resultados = new ArrayList<>();
+        
+        //Genera y guarda las permutaciones
+        generarPermutacionesRecursivo(setCaracters, new ArrayList<>(), resultados);
 
-        generarPermutacionesRecursivo(set, new ArrayList<>(), result);
-
-        return result;
+        return resultados;
     }
 
-    public static void generarPermutacionesRecursivo(HashSet<Character> set, ArrayList<Character> permutacionActual, ArrayList<String> result) {
-    
-        if (permutacionActual.size() == set.size()) {
+    public static void generarPermutacionesRecursivo(HashSet<Character> setCaracters, ArrayList<Character> permutacionActual, ArrayList<String> resultados) {
+        
+        //Construye las permutaciones de permutacionActual
+        StringBuilder permutacionBuilder = new StringBuilder();
 
-            StringBuilder permutationBuilder = new StringBuilder();
+        if (permutacionActual.size() == setCaracters.size()) {
 
             for (Character c : permutacionActual) {
-                permutationBuilder.append(c);
+                permutacionBuilder.append(c);
             }
 
-            result.add(permutationBuilder.toString());
+            //Añade la permutacion al arraylist de resultados
+            resultados.add(permutacionBuilder.toString());
 
         } else {
-            for (Character c : set) {
+            for (Character c : setCaracters) {
                 
-                if (!permutacionActual.contains(c)) {
+                
+                if (!permutacionActual.contains(c)) { //evita que se añáda un caracter ya existente
 
                     permutacionActual.add(c);
-                    generarPermutacionesRecursivo(set, permutacionActual, result);
+                    generarPermutacionesRecursivo(setCaracters, permutacionActual, resultados);
+
+                    //Elimina la permutacion actual del arraylist para crear otra permutacion
                     permutacionActual.remove(permutacionActual.size() - 1);
 
                 }
